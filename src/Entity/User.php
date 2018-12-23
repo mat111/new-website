2a0a37358b938b00d57e4d3aeac1c2d50f433782
matabgaryan <matabgaryan1@gmail.com>
2018-12-23 22:41:36 +0400

    FEAT:User.Ex-1292. User flow functional

---
+++ src/Entity/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
---
+++ src/Manager/UserManager.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Manager;
+
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+
+class UserManager
+{
+    /**
+     * @var EntityManager $em
+     */
+    private $em;
+
+    /**
+     * @var UserRepository $repository
+     */
+    private $repository;
+
+    /**
+     * UserManager constructor.
+     * @param EntityManager $em
+     * @param UserRepository $repository
+     */
+    public function __construct(EntityManager $em, UserRepository $repository)
+    {
+        $this->em = $em;
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param $id
+     * @return \App\Entity\User|null
+     */
+    public function find($id)
+    {
+        return $this->repository->find($id);
+    }
+
+    /**
+     * @return User[]
+     */
+    public function findAll()
+    {
+        return $this->repository->findAll();
+    }
+
+    /**
+     * @return User[]
+     */
+    public function getPrefixedUsers()
+    {
+        $users = $this->findAll();
+
+        /** @var User $user */
+        foreach ($users as $user){
+            $user->setName('_'.$user->getName());
+        }
+
+        return $users;
+    }
+
+    /**
+     * @param User $user
+     * @return User
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function persist(User $user)
+    {
+        $this->em->persist($user);
+        $this->em->flush();
+
+        return $user;
+    }
+
+    /**
+     * @param User $user
+     * @return null
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function remove(User $user)
+    {
+        $this->em->remove($user);
+        $this->em->flush();
+
+        return null;
+    }
+}
